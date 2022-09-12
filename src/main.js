31eefb4959e2a0f55ce42f0f9f892e79ea75503f
Marcelo Chaves <9marceli@gmail.com>
2022-09-12 17:24:01 -0300

    Creation of a SPA using Vue.js as a basis for the Alura course project using Vue.js

---
+++ src/main.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueResource from 'vue-resource';
+import VueRouter from 'vue-router';
+import { routes } from './routes';
+import './directives/Transform';
+import VeeValidate from 'vee-validate'
+import msg from './pt_BR'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
+import 'jquery/dist/jquery'
+
+Vue.use(VueResource);
+Vue.http.options.root = 'http://localhost:3000';
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+});
+
+Vue.use(VeeValidate, {
+  locale: 'pt_BR',
+  dictionary: {
+    pt_BR: {
+      messages: msg
+    }
+  }
+})
+
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
+
